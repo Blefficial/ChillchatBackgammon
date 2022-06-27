5cb9f64e8d349718aba5f5d6cfb17da9a84822de
Blefficial <toohveli@gmail.com>
2022-06-27 05:02:29 +0300

    Added sprite for checkers and background for board

--- src/Board.cs
+++ src/Board.cs
@@ -5,65 +5,74 @@ using System.Collections.Generic;
 public class Board : Node2D
 {
 	BoardRenderer _renderer;
+	Label _diceLabel;
 	RandomNumberGenerator _rng;
 
 	private int[,] _boardPoints = new int[24, 5];
-	public int[,] BoardPoints {
-		get { return _boardPoints; }
-	}
-	//private int[] _moves 		= new int[4];
+	public int[,] BoardPoints 	{ get { return _boardPoints; } }
+	private int[] _wall			= new int[20];
+	public int[] Wall 			{ get {return _wall; } }
 	private List<int> _moves 	= new List<int>();
 	private int activeColumn 	= -1;
-	private int _activePlayer = 1;
+	private int _activePlayer 	= 1;
+	private int[] _helperMove	= new int[2] { -1, -1 };
+
+	[Signal]
+	delegate void OnBoardUpdate();
 
 	public override void _Ready()
 	{
-		AddPiece(1, 11);
-		AddPiece(1, 11);
-		AddPiece(1, 11);
-		AddPiece(1, 11);
-		AddPiece(1, 11);
+		_AddPiece(1, 11);
+		_AddPiece(1, 11);
+		_AddPiece(1, 11);
+		_AddPiece(1, 11);
+		_AddPiece(1, 11);
 
-		AddPiece(1, 18);
-		AddPiece(1, 18);
-		AddPiece(1, 18);
-		AddPiece(1, 18);
-		AddPiece(1, 18);
+		_AddPiece(1, 18);
+		_AddPiece(1, 18);
+		_AddPiece(1, 18);
+		_AddPiece(1, 18);
+		_AddPiece(1, 18);
 
-		AddPiece(1, 16);
-		AddPiece(1, 16);
-		AddPiece(1, 16);
+		_AddPiece(1, 16);
+		_AddPiece(1, 16);
+		_AddPiece(1, 16);
 
-		AddPiece(1, 0);
-		AddPiece(1, 0);
+		_AddPiece(1, 0);
+		_AddPiece(1, 0);
 
-		AddPiece(2, 5);
-		AddPiece(2, 5);
-		AddPiece(2, 5);
-		AddPiece(2, 5);
-		AddPiece(2, 5);
+		_AddPiece(2, 5);
+		_AddPiece(2, 5);
+		_AddPiece(2, 5);
+		_AddPiece(2, 5);
+		_AddPiece(2, 5);
 
-		AddPiece(2, 12);
-		AddPiece(2, 12);
-		AddPiece(2, 12);
-		AddPiece(2, 12);
-		AddPiece(2, 12);
+		_AddPiece(2, 12);
+		_AddPiece(2, 12);
+		_AddPiece(2, 12);
+		_AddPiece(2, 12);
+		_AddPiece(2, 12);
 
-		AddPiece(2, 7);
-		AddPiece(2, 7);
-		AddPiece(2, 7);
+		_AddPiece(2, 7);
+		_AddPiece(2, 7);
+		_AddPiece(2, 7);
 
-		AddPiece(2, 23);
-		AddPiece(2, 23);
+		_AddPiece(2, 23);
+		_AddPiece(2, 23);
 
 		_renderer 	= GetNode<BoardRenderer>("BoardRenderer") as BoardRenderer;
+		_diceLabel 	= GetNode<Label>("DiceLabel") as Label;
 		_rng		= new RandomNumberGenerator();
+
+		_wall[0] = 1;
+		_wall[10] = 2;
 
 		ThrowDice();
 		HasValidMove(this._activePlayer, _moves.ToArray());
+		EmitSignal("OnBoardUpdate");
 	}
 
-	private int GetTopPiece(int column) 
+	private int _GetTopPiece(int column) 
 	{
 		for (int i = 4; i >= 0; i--) 
 		{
@@ -73,7 +82,7 @@ public class Board : Node2D
 		return -1;
 	}
 
-	public int GetNextFreeSlot(int column) 
+	private int _GetNextFreeSlot(int column) 
 	{
 		for (int i = 0; i < 5; i++)
 		{
@@ -83,7 +92,48 @@ public class Board : Node2D
 		return -1;
 	}
 
-	public void AddPiece(int player, int column) 
+	private int _GetTopWallPiece(int player) 
+	{
+		int count = player == 1 ? 0 : 10;
+		for (int i = count + 9; i >= count; i--) 
+		{
+ 			if (_wall[i] != 0) return i;
+		}
+
+		return -1;
+	}
+
+	private int _GetNextFreeWallSlot(int player)
+	{
+		if (player == 1) {
+			for (int slot = 0; slot < 10; slot++) {
+				if (_wall[slot] == 0) return slot;
+			}
+		}
+		else
+		{
+			for (int slot = 10; slot < 20; slot++) {
+				if (_wall[slot] == 0) return slot;
+			}
+		}
+
+		return -1;
+	}
+
+	private void _SetHelperMove(int column, int position)
+	{
+		_helperMove[0] = column;
+		_helperMove[1] = position;
+		GD.Print("Set helper move " + column + ", " + position);
+		_renderer.Update();
+	}
+
+	public bool IsHelperMove(int column, int position)
+	{
+		return _helperMove[0] == column && _helperMove[1] == position;
+	}
+
+	private void _AddPiece(int player, int column)
 	{
 		if (column > 23)
 		{
@@ -91,51 +141,65 @@ public class Board : Node2D
 			return;
 		}
 
-		int freeSlot = GetNextFreeSlot(column);
-
+		int freeSlot = _GetNextFreeSlot(column);
 		if (freeSlot == -1) return;
 
 		_boardPoints[column, freeSlot] = player;
 	}
 
-	public void ReplacePiece(int fromColumn, int toColumn) {
-		int topPiece = GetTopPiece(fromColumn);
+	private void _AddPieceToWall(int player)
+	{
+		int nextFreeWallSlot 	= _GetNextFreeWallSlot(player);
+		_wall[nextFreeWallSlot] = player;
+		_renderer.Update();
+	}
+
+	private void _ReplacePiece(int fromColumn, int toColumn)
+	{
+		int topPiece = _GetTopPiece(fromColumn);
 		int piece 	 = _boardPoints[fromColumn, topPiece];
 
 		_boardPoints[toColumn, 0] 			= piece;
 		_boardPoints[fromColumn, topPiece] 	= 0;
-		_renderer.Update();
+		_AddPieceToWall(3 - _activePlayer);
 	}
 
-	public void MovePiece(int fromColumn, int toColumn)
+	private void _MovePiece(int fromColumn, int toColumn)
 	{
-		int topPiece = GetTopPiece(fromColumn);
-		int freeSlot = GetNextFreeSlot(toColumn);
+		int topPiece = _GetTopPiece(fromColumn);
+		int freeSlot = _GetNextFreeSlot(toColumn);
 		int piece 	 = _boardPoints[fromColumn, topPiece];
 
 		_boardPoints[toColumn, freeSlot] 	= piece;
 		_boardPoints[fromColumn, topPiece] 	= 0;
-		_renderer.Update();
 	}
-	public bool IsValidMove(int player, int fromColumn, int destinationColumn) 
-	{
-		return false;
+
+	private void _UpdateDiceLabel() {
+		String rolls = "Player" + _activePlayer + ": ";
+		foreach(int move in _moves) 
+			{
+				rolls+= String.Format(" [{0,1:D}] ", move);
+			}
+		_diceLabel.Text = rolls;
 	}
 	
-	public void ThrowDice() {
+	public void ThrowDice()
+	{
 		_rng.Randomize();
 		int dice1 = _rng.RandiRange(1, 6);
 
 		_rng.Randomize();
 		int dice2 = _rng.RandiRange(1, 6);
 
-		if (dice1 == dice2) {
+		if (dice1 == dice2) 
+		{
 			_moves.Add(dice1);
 			_moves.Add(dice1);
 			_moves.Add(dice1);
 			_moves.Add(dice1);
 
 			GD.Print(String.Format("Player {0,1:D} rolled the dice [{1,1:D}], [{2,1:D}], [{3,1:D}], [{4,1:D}]", _activePlayer, _moves[0], _moves[1], _moves[2], _moves[3]));
+			_UpdateDiceLabel();
 			return;
 		} 
 
@@ -143,17 +207,120 @@ public class Board : Node2D
 		_moves.Add(dice2);
 
 		GD.Print(String.Format("Player {0,1:D} rolled the dice [{1,1:D}], [{2,1:D}]", _activePlayer, _moves[0], _moves[1]));
+		_UpdateDiceLabel();
 	}
 
-	public bool HasValidMove(int player, int[] moves) {
+	private bool _IsWallOccupied(int player)
+	{
+		return player == 1 ? _GetNextFreeWallSlot(player) > 0 : _GetNextFreeWallSlot(player) > 10;
+	}
+
+	public int[] GetFirstValidMove(int player, int[] moves)
+	{
 		int direction = player == 1 ? 1 : -1;
 
-		foreach	(int amount in moves) {
+		if (_IsWallOccupied(player))
+		{
+			foreach (int amount in moves)
+			{
+				int column  = player == 1 ? amount - 1 : 24 - amount;
+				int topPieceIndex 	= _GetTopPiece(column);
+				if (topPieceIndex == -1)
+				{
+					return new int[] { column, 0 };
+				}
+				
+				if (topPieceIndex >= 4) continue;
+
+				if (_boardPoints[column, topPieceIndex] == player)
+				{
+					return new int[] { column, topPieceIndex + 1 };
+				}
+
+				if (topPieceIndex == 0)
+				{
+					return new int[] { column, 0 };
+				}
+			}
+
+			return new int[2] { -1, -1 };
+		}
+
+		foreach	(int amount in moves)
+		 {
 			if (amount == 0) continue;
 
 			for (int column = 0; column < _boardPoints.GetLength(0); column++)
 			{
-				int topPieceIndex = GetTopPiece(column);
+				int topPieceIndex = _GetTopPiece(column);
+				if (topPieceIndex == -1) continue;
+
+				if (_boardPoints[column, topPieceIndex] != player) continue;
+
+				int move = amount * direction;
+				int targetColumn = column + move;
+				if (targetColumn > _boardPoints.GetLength(0) || targetColumn < 0) continue; // TODO: Add bearing off feature.
+
+				topPieceIndex = _GetTopPiece(targetColumn);
+				if (topPieceIndex < 0) 
+				{
+					return new int[] { targetColumn, 0 };
+				} 
+
+				if (_boardPoints[targetColumn, topPieceIndex] == player)
+				{
+					if (topPieceIndex >= 4) continue;
+
+					return new int[] { targetColumn, topPieceIndex + 1 };
+				} 
+			}
+		}
+		
+		return new int[2] { -1, -1 };
+	}
+
+	public bool HasValidMove(int player, int[] moves)
+	{
+		int direction = player == 1 ? 1 : -1;
+
+		if (_IsWallOccupied(player)) // Player has pieces on the wall and cannot move any other pieces.
+		{
+			foreach (int amount in moves)
+			{
+				int column  = player == 1 ? amount - 1 : 24 - amount;
+				int topPieceIndex 	= _GetTopPiece(column);
+				if (topPieceIndex == -1)
+				{
+					GD.Print(String.Format("Found a valid move WALL->{0:D}", column));
+					return true;
+				}
+				
+				if (topPieceIndex >= 4) continue;
+
+				if (_boardPoints[column, topPieceIndex] == player)
+				{
+					GD.Print(String.Format("Found a valid move WALL->{0:D}", column));
+					return true;
+				}
+
+				if (topPieceIndex == 0)
+				{
+					GD.Print(String.Format("Found a valid move WALL->{0:D}", column));
+					return true;
+				}
+			}
+
+			GD.Print("Failed to find a valid move");
+			return false;
+		}
+
+		foreach	(int amount in moves)
+		 {
+			if (amount == 0) continue;
+
+			for (int column = 0; column < _boardPoints.GetLength(0); column++)
+			{
+				int topPieceIndex = _GetTopPiece(column);
 				if (topPieceIndex == -1) continue; // Move onto next column if this column contains no pieces.
 
 				if (_boardPoints[column, topPieceIndex] != player) continue; // Move onto next column if the piece is not owned by player.
@@ -161,8 +328,8 @@ public class Board : Node2D
 				int move = amount * direction;
 				if (column + move > _boardPoints.GetLength(0) || column + move < 0) continue; // TODO: Add bearing off feature.
 
-				topPieceIndex = GetTopPiece(column + move);
-				if (topPieceIndex < 0) 
+				topPieceIndex = _GetTopPiece(column + move);
+				if (topPieceIndex < 0)
 				{
 					GD.Print(String.Format("Found a valid move {0:D}->{1:D}", column, column + move));
 					return true; // If the target column is empty or contains only one opponent piece, in which case the target piece can be moved there.
@@ -170,6 +337,8 @@ public class Board : Node2D
 
 				if (_boardPoints[column + move, topPieceIndex] == player)
 				{
+					if (topPieceIndex >= 4) continue; // Target column is full.
+
 					GD.Print(String.Format("Found a valid move {0:D}->{1:D}", column, column + move));
 					return true; // If the top piece is owned by the player, the piece can be moved there.
 				} 
@@ -192,13 +361,14 @@ public class Board : Node2D
 	private void _UseMove(int pointsMoved) 
 	{
 		_moves.Remove(pointsMoved);
+		_UpdateDiceLabel();
 	}
 
-	private void _OnSuccessfulMove(int fromColumn, int toColumn) 
+	private void _OnSuccessfulMove(int distance)
 	{
-		int pointsMoved = Math.Abs(toColumn - fromColumn);
-		_UseMove(pointsMoved);
-
+		_SetHelperMove(-1, -1);
+		_renderer.Update();
+		_UseMove(distance);
 		string movesLeft = "Player" + this._activePlayer + " has " + _moves.Count + " move(s) left:";
 
 		foreach (int move in _moves)
@@ -212,14 +382,22 @@ public class Board : Node2D
 		{
 			_ChangeTurn();
 		}
+
+		EmitSignal("OnBoardUpdate");
+	}
+
+	private void _OnSuccessfulMove(int fromColumn, int toColumn) 
+	{
+		int pointsMoved = Math.Abs(toColumn - fromColumn);
+		_OnSuccessfulMove(pointsMoved);
 	}
 
 	public bool CanReach(int fromColumn, int toColumn)
 	{
-
 		if (this._activePlayer == 1)
 		{
-			if (fromColumn > toColumn) {
+			if (fromColumn > toColumn) 
+			{
 				GD.Print("Unable to reach: Player 1 must move clockwise.");
 				return false;
 			}
@@ -240,6 +418,75 @@ public class Board : Node2D
 		return false;
 	}
 
+	private void _MoveFromWall(int toColumn)
+	{
+		int opposingPlayer = 3 - _activePlayer;
+		if (_boardPoints[toColumn, 0] == opposingPlayer)
+		{
+			_boardPoints[toColumn, 0] = 0;
+			_AddPieceToWall(opposingPlayer);
+		}
+		
+		int freeSlot = _GetNextFreeSlot(toColumn);
+
+		_boardPoints[toColumn, freeSlot] 		= _activePlayer;
+		_wall[_GetTopWallPiece(_activePlayer)] 	= 0;
+	}
+
+	private void _AttemptMoveFromWall(int toColumn)
+	{
+		int distanceToColumn = Mathf.Abs(_activePlayer == 1 ? toColumn + 1 : toColumn - 24);
+		bool canReach 		 = false;
+
+		foreach (int move in _moves)
+		{
+			if (distanceToColumn == move)
+			{
+				canReach = true;
+				break;
+			}
+		}
+
+		if (!canReach)
+		{
+			GD.Print("Unable to move from wall, no moves left matched the attempted move.");
+			return;
+		}
+
+		int topPieceIndex = _GetTopPiece(toColumn);
+		if (topPieceIndex > 4)
+		{
+			GD.Print("Unable to move from wall, target column is full.");
+			return;
+		};
+
+		if (topPieceIndex == -1)
+		{
+			GD.Print("Move success: WALL->" + toColumn);
+			_MoveFromWall(toColumn);
+			_OnSuccessfulMove(distanceToColumn);
+			return;
+		}
+
+		if (topPieceIndex == 0)
+		{
+			GD.Print("Move success: WALL->" + toColumn);
+			_MoveFromWall(toColumn);
+			_OnSuccessfulMove(distanceToColumn);
+			return;
+		}
+
+		if (_boardPoints[toColumn, topPieceIndex] == _activePlayer)
+		{
+			GD.Print("Move success: WALL->" + toColumn);
+			_MoveFromWall(toColumn);
+			_OnSuccessfulMove(distanceToColumn);
+			return;
+		}
+
+		GD.Print("Failed to move from wall");
+	}
+
 	public void AttemptMove(int fromColumn, int toColumn) 
 	{
 		if (fromColumn == toColumn) return;
@@ -250,7 +497,7 @@ public class Board : Node2D
 			return;
 		}
 
-		int topPieceIndex = GetTopPiece(fromColumn);
+		int topPieceIndex = _GetTopPiece(fromColumn);
 		if (topPieceIndex == -1) {
 			GD.Print("Move failed: No piece in column: " + fromColumn);
 			return;
@@ -262,11 +509,11 @@ public class Board : Node2D
 			return;
 		}
 
-		topPieceIndex = GetTopPiece(toColumn);
+		topPieceIndex = _GetTopPiece(toColumn);
 		if (topPieceIndex < 0)
 		{
 			GD.Print("Move success: " + fromColumn + "->" + toColumn);
-			MovePiece(fromColumn, toColumn);
+			_MovePiece(fromColumn, toColumn);
 			_OnSuccessfulMove(fromColumn, toColumn);
 			return;
 		}
@@ -276,7 +523,7 @@ public class Board : Node2D
 			if (topPieceIndex == 0)
 			{
 				GD.Print("Move success: " + fromColumn + "->" + toColumn);
-				ReplacePiece(fromColumn, toColumn);
+				_ReplacePiece(fromColumn, toColumn);
 				_OnSuccessfulMove(fromColumn, toColumn);
 				return;
 			}
@@ -287,7 +534,7 @@ public class Board : Node2D
 			if (topPieceIndex < 4)
 			{
 				GD.Print("Move success: " + fromColumn + "->" + toColumn);
-				MovePiece(fromColumn, toColumn);
+				_MovePiece(fromColumn, toColumn);
 				_OnSuccessfulMove(fromColumn, toColumn);
 				return;
 			}
@@ -300,18 +547,44 @@ public class Board : Node2D
 	{
 		if (Input.IsActionJustPressed("mouse_left_button"))
 		{
-			float mouseX 		= GetViewport().GetMousePosition().x;
-			float mouseY 		= GetViewport().GetMousePosition().y;
-			this.activeColumn 	= _renderer.getColumn(mouseX, mouseY);
+			Vector2 mousePosition = GetViewport().GetMousePosition();
+
+			if (_IsWallOccupied(_activePlayer))
+			{
+				if (_renderer.IsMouseInsideWall(mousePosition.x, mousePosition.y))
+				{
+					this.activeColumn = -2;
+					return;
+				}
+			}
+
+			this.activeColumn = _renderer.getColumn(mousePosition.x, mousePosition.y);
 		}
 
 		if (Input.IsActionJustReleased("mouse_left_button"))
 		{
-			float mouseX 		= GetViewport().GetMousePosition().x;
-			float mouseY 		= GetViewport().GetMousePosition().y;
-			int targetColumn 	= _renderer.getColumn(mouseX, mouseY);
-			
+			Vector2 mousePosition 	= GetViewport().GetMousePosition();
+			int targetColumn 		= _renderer.getColumn(mousePosition.x, mousePosition.y);
+
+			if (_IsWallOccupied(_activePlayer))
+			{
+				if (this.activeColumn == -2)
+				{
+					_AttemptMoveFromWall(targetColumn);
+					return;
+				}
+
+				GD.Print("Player must move a piece from the wall!");
+				return;
+			}
+
 			AttemptMove(this.activeColumn, targetColumn);
+		}
+
+		if (Input.IsActionJustPressed("show_help"))
+		{
+			int[] move = GetFirstValidMove(this._activePlayer, _moves.ToArray());
+			_SetHelperMove(move[0], move[1]);
 		}
 	}
 }
--- src/BoardRenderer.cs
+++ src/BoardRenderer.cs
@@ -2,40 +2,55 @@ using Godot;
 
 public class BoardRenderer : Node2D {
 
-	private static Color RED 			= new Color(1, 0, 0);
-	private static Color BLACK 			= new Color(0, 0, 0);
-	private static float PADDING 		= 1;
-	private static float SPACER_HEGIHT 	= 25;
-	private static float SPACER_WIDTH 	= 50;
-	private static Vector2 CELL_SIZE 	= new Vector2(70, 70);
-	private static Vector2 BOARD_SIZE 	= new Vector2(12 * (CELL_SIZE.x + PADDING) + SPACER_WIDTH, 10 * (CELL_SIZE.y + PADDING) + SPACER_HEGIHT);
+	private static Color RED 			= new Color(1, 0.4f, 0.4f);
+	private static Color WHITE 			= new Color(1, 1, 1);
+	private static Color GREY 			= new Color(0.5f, 0.5f, 0.5f, 0.8f);
+	private static float SPACER_HEIGHT 	= 35;
+	private static float SPACER_WIDTH 	= 72;
+	private static Vector2 PADDING 		= new Vector2(11.5f, 0);
+	private static Vector2 CELL_SIZE 	= new Vector2(90, 90);
+	private static Vector2 BOARD_SIZE 	= new Vector2(12 * (CELL_SIZE.x + PADDING.x) + SPACER_WIDTH, 10 * (CELL_SIZE.y + PADDING.y) + SPACER_HEIGHT);
+	private static Texture CHECKER		= ResourceLoader.Load<Texture>("res://assets/images/checker/checker.png") as Texture;
 	private Board _board;
+	private Sprite[] _checkerSprites 	= new Sprite[140];
 
 	public override void _Ready()
 	{
 		_board = GetParent<Board>() as Board;
+		_board.Connect("OnBoardUpdate", this, "_UpdateBoard");
+
+		_Setup(_board.BoardPoints, _board.Wall);
+		Update();
 	}
 
-	public void RenderBoard(int[,] _points) 
+	private void _Setup(int[,] points, int[] wall)
 	{
-		for (int column = 0; column < _points.GetLength(0); column++) 
+		int spriteWidth						= CHECKER.GetWidth();
+		int spriteHeight					= CHECKER.GetHeight();
+		Vector2 spriteScale					= new Vector2(CELL_SIZE.x / spriteWidth, CELL_SIZE.y / spriteHeight);
+
+		for (int column = 0; column < points.GetLength(0); column++)
 		{
-			for (int position = 0; position < _points.GetLength(1); position++) 
+			for (int position = 0; position < points.GetLength(1); position++) 
 			{
-				if (_points[column, position] == 0) continue;
+				Sprite checkerSprite 					= new Sprite();
+				checkerSprite.Texture 					= CHECKER;
+				checkerSprite.Visible 					= false;
+				checkerSprite.Offset					= new Vector2(spriteWidth * 0.5f, spriteHeight * 0.5f);
+				checkerSprite.Scale						= spriteScale;
+				_checkerSprites[column * 5 + position]  = checkerSprite;
+				this.AddChild(checkerSprite);
 
-				Color color = _points[column, position] == 1 ? RED : BLACK;
-
-				float xOffset = SPACER_WIDTH;
+				float xOffset 	= SPACER_WIDTH;
 
 				if (column < 12)
 				{	
-					if (column < 6) 
+					if (column < 6)
 					{
 						xOffset = 0;
 					}
 
-					DrawRect(new Rect2(new Vector2(BOARD_SIZE.x - (column + 1) * (CELL_SIZE.x + PADDING) - xOffset, BOARD_SIZE.y - (position + 1) * (CELL_SIZE.y + PADDING) + SPACER_HEGIHT), CELL_SIZE), color);
+					checkerSprite.Position 		= new Vector2(BOARD_SIZE.x - (column + 1) * (CELL_SIZE.x + PADDING.x) - xOffset, BOARD_SIZE.y - (position + 1) * (CELL_SIZE.y + PADDING.y));
 					continue;
 				}
 
@@ -44,19 +59,119 @@ public class BoardRenderer : Node2D {
 					xOffset = 2 * SPACER_WIDTH;
 				}
 
-				DrawRect(new Rect2(new Vector2(column * (CELL_SIZE.x + PADDING) - BOARD_SIZE.x + xOffset, position * (CELL_SIZE.y + PADDING)), CELL_SIZE), color);
+				checkerSprite.Position 			= new Vector2(column * (CELL_SIZE.x + PADDING.x) - BOARD_SIZE.x + xOffset, position * (CELL_SIZE.y + PADDING.y));
 			}
+		}
+		
+		for (int piece = 0; piece < wall.GetLength(0); piece++)
+		{
+			Sprite checkerSprite 					= new Sprite();
+			checkerSprite.Texture 					= CHECKER;
+			checkerSprite.Visible 					= false;
+			checkerSprite.Offset					= new Vector2((spriteWidth * 0.8f) * 0.5f, (spriteHeight * 0.8f) * 0.5f);
+			checkerSprite.Scale						= spriteScale * 1.2f;
+			_checkerSprites[120 + piece]  			= checkerSprite;
+			this.AddChild(checkerSprite);
+
+			if (piece < 10)
+			{
+				checkerSprite.Position 				= new Vector2(BOARD_SIZE.x * 0.5f - CELL_SIZE.x * 0.5f - PADDING.x * 0.5f, BOARD_SIZE.y - CELL_SIZE.y - piece * (CELL_SIZE.y + PADDING.y));
+				continue;
+			}
+
+			checkerSprite.Position					= new Vector2(BOARD_SIZE.x * 0.5f - CELL_SIZE.x * 0.5f - PADDING.x * 0.5f, (piece - 10) * (CELL_SIZE.y + PADDING.y));
 		}
 	}
 
-	public override void _Draw()
+	private void _UpdateBoard()
 	{
-		RenderBoard(_board.BoardPoints);
+		for (int column = 0; column < _board.BoardPoints.GetLength(0); column++)
+		{
+			for (int position = 0; position < _board.BoardPoints.GetLength(1); position++)
+			{
+				Sprite checkerSprite 				= _checkerSprites[column * 5 + position];
+				checkerSprite.Visible				= _board.BoardPoints[column, position] > 0;
+				checkerSprite.SelfModulate			= _board.BoardPoints[column, position] == 1 ? RED : WHITE;
+			}
+		}
+
+		for (int piece = 0; piece < _board.Wall.GetLength(0); piece++)
+		{
+			Sprite checkerSprite 					= _checkerSprites[120 + piece];
+			checkerSprite.Visible 					= _board.Wall[piece] > 0;
+			checkerSprite.SelfModulate				= _board.Wall[piece] == 1 ? RED : WHITE;
+		}
 	}
 
-	public bool isPositionOnBoard(float mouseX, float mouseY) 
+	public void RenderBoardCells(int[,] points, int[] wall) 
 	{
-		return this.Position.x < mouseX && mouseX < this.Position.x + BOARD_SIZE.x && this.Position.y < mouseY && mouseY < this.Position.y + BOARD_SIZE.y;
+		for (int column = 0; column < points.GetLength(0); column++)
+		{
+			for (int position = 0; position < points.GetLength(1); position++) 
+			{
+				Color color 	= points[column, position] == 1 ? RED : WHITE;
+				bool isHelper 	= _board.IsHelperMove(column, position);
+				if (points[column, position] == 0)
+				{
+					if (!isHelper) continue;
+					color = GREY; // For drawing the helper ghost piece. Placeholder for now.
+				}
+				
+				float xOffset 	= SPACER_WIDTH;
+
+				if (column < 12)
+				{	
+					if (column < 6)
+					{
+						xOffset = 0;
+					}
+
+					DrawRect(new Rect2(new Vector2(BOARD_SIZE.x - (column + 1) * (CELL_SIZE.x + PADDING.x) - xOffset, BOARD_SIZE.y - (position + 1) * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), color);
+					
+					if (isHelper)
+					{
+						DrawRect(new Rect2(new Vector2(BOARD_SIZE.x - (column + 1) * (CELL_SIZE.x + PADDING.x) - xOffset, BOARD_SIZE.y - (position + 1) * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), GREY);
+					}
+					
+					continue;
+				}
+
+				if (column > 17) 
+				{
+					xOffset = 2 * SPACER_WIDTH;
+				}
+
+				DrawRect(new Rect2(new Vector2(column * (CELL_SIZE.x + PADDING.x) - BOARD_SIZE.x + xOffset, position * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), color);
+
+				if (isHelper)
+				{
+					DrawRect(new Rect2(new Vector2(column * (CELL_SIZE.x + PADDING.x) - BOARD_SIZE.x + xOffset, position * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), GREY);
+				}
+			}
+		}
+
+		for (int piece = 0; piece < wall.GetLength(0); piece++)
+		{
+			if (wall[piece] == 0) continue;
+			Color color = wall[piece] == 1 ? RED : WHITE;
+			
+			if (piece < 10)
+			{
+				DrawRect(new Rect2(new Vector2(BOARD_SIZE.x * 0.5f - CELL_SIZE.x * 0.5f - PADDING.x * 0.5f, BOARD_SIZE.y - CELL_SIZE.y - piece * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), color);
+				continue;
+			}
+
+			DrawRect(new Rect2(new Vector2(BOARD_SIZE.x * 0.5f - CELL_SIZE.x * 0.5f - PADDING.x * 0.5f, (piece - 10) * (CELL_SIZE.y + PADDING.y)), CELL_SIZE), color);
+		}
+	}
+
+	public bool IsMouseInsideWall(float mouseX, float mouseY)
+	{
+		float leftBorder 	= _board.Position.x + BOARD_SIZE.x * 0.5f - SPACER_WIDTH * 0.5f;
+		float rightBorder 	= _board.Position.x + BOARD_SIZE.x * 0.5f + SPACER_WIDTH * 0.5f;
+		float bottomBorder 	= _board.Position.y + BOARD_SIZE.y;
+
+		return mouseX > leftBorder && mouseX < rightBorder && mouseY > _board.Position.y && mouseY < bottomBorder; 
 	}
 
 	public int getColumn(float mouseX, float mouseY)
@@ -66,15 +181,16 @@ public class BoardRenderer : Node2D {
 		float halfWidth 	= BOARD_SIZE.x * 0.5f;
 		float xOffset 		= 0;
 
-		if (mouseX >  halfWidth) {
+		if (mouseX > _board.Position.x + halfWidth)
+		{
 			xOffset = SPACER_WIDTH;
 		}
 
-		column = 12 - Mathf.CeilToInt(((mouseX - xOffset) / (BOARD_SIZE.x - SPACER_WIDTH)) * 12);
+		column = 12 - Mathf.CeilToInt((( Mathf.Max(mouseX - xOffset - _board.Position.x, 0)) / (BOARD_SIZE.x - SPACER_WIDTH)) * 12);
 
-		if (mouseY < halfHeight)
+		if (mouseY < _board.Position.y + halfHeight)
 		{	
-			column = 12 + Mathf.FloorToInt(((mouseX - xOffset) / (BOARD_SIZE.x - SPACER_WIDTH)) * 12);
+			column = 12 + Mathf.FloorToInt((( Mathf.Max(mouseX - xOffset - _board.Position.x, 0)) / (BOARD_SIZE.x - SPACER_WIDTH)) * 12);
 		}
 
 		return Mathf.Clamp( column, 0, 23 );
