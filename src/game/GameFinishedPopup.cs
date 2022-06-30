9f31db712e651ee4cd2ddbb90f4fbfc4495c9815
Blefficial <toohveli@gmail.com>
2022-07-01 01:54:03 +0300

    Added players' total points to after-game screen

--- src/game/GameFinishedPopup.cs
+++ src/game/GameFinishedPopup.cs
@@ -4,18 +4,27 @@ using System;
 public class GameFinishedPopup : Popup
 {
 	private Label _winnerLabel;
+	private Label _player1PointsLabel;
+	private Label _player2PointsLabel;
 	
 	[Signal]
 	delegate void RequestRematch();
 
 	public override void _Ready()
 	{
-		_winnerLabel = GetNode<Label>("VBoxContainer/WinnerLabel");
+		_winnerLabel 		= GetNode<Label>("VBoxContainer/WinnerLabel") 			as Label;
+		_player1PointsLabel = GetNode<Label>("VBoxContainer/Player1PointsLabel") 	as Label;
+		_player2PointsLabel = GetNode<Label>("VBoxContainer/Player2PointsLabel") 	as Label;
 	}
 
-	public void UpdateWinnerLabel(string player, string victoryTitle, int scoreGained)
+	public void UpdateWinnerLabel(string winner, string victoryTitle, int scoreGained)
 	{
-		_winnerLabel.Text = String.Format("{0} won and scored {1} (+{2:D}) points!", player, victoryTitle, scoreGained);
+		string player1 = Constants.PLAYER_1;
+		string player2 = Constants.PLAYER_2;
+		
+		_winnerLabel.Text = String.Format("{0} won and scored {1} (+{2:D}) points!", winner, victoryTitle, scoreGained);
+		_player1PointsLabel.Text = String.Format("{0} now has {1:D} points in total!", player1, PlayerData.PLAYER_STATS[player1]["total_points"]);
+		_player2PointsLabel.Text = String.Format("{0} now has {1:D} points in total!", player2, PlayerData.PLAYER_STATS[player2]["total_points"]);
 	}
 
 	public override void _Process(float delta)
